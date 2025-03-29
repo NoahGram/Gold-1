9497acbfc12971d51a51a4faf53cd102472b5ff3
MuffinMan <91591598+NoahGram@users.noreply.github.com>
2025-03-29 20:31:01 +0100

    Removed Double Move_Slide()
    In Falling there was a additional move_and_slide()

--- Scripts/States/falling.gd
+++ Scripts/States/falling.gd
@@ -1,21 +1,19 @@
 extends State
 class_name Falling
 
-const FALL_SPEED = 800.0
+const FALL_MULTIPLIER = 2.5
 
 func enter():
 	if animation_player:
 		animation_player.play("Fall")
 
 func physics_update(delta: float):
-	player.velocity.y += FALL_SPEED * delta
+	player.velocity.y += player.GRAVITY * FALL_MULTIPLIER * delta  
 
 	var input_dir = Input.get_axis("left", "right")
 	player.velocity.x = input_dir * 100.0
 	
 	animation_player.flip_h = player.velocity.x < 0
-
-	player.move_and_slide()
 
 	if player.is_on_floor():
 		if input_dir != 0:
